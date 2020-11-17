3973368fc2187afff1335a1de018cfbc6dee3163
Ziyuan <zxu25@slb.com>
2020-11-17 22:04:31 +0800

    feat: add tasks image

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -2,24 +2,30 @@ import { loadAllItems, loadPromotions } from './Dependencies'
 
 export function printReceipt(tags: string[]): string {
   const items = loadAllItems()
-  const receiptMap = new Map()
+  const receiptMap = new Map<string, Item>()
   for (const inputTag of tags) {
-    const tag = inputTag.split('-')[0]
+    const tag: string = inputTag.split('-')[0]
     const quantity = inputTag.split('-').length > 1 ? Number.parseFloat(inputTag.split('-')[1]) : 1
     if (receiptMap.has(tag)) {
-      receiptMap.get(tag).quantity += quantity
+      receiptMap.get(tag)!.quantity += quantity
     }
     else {
-      const item = items.find(item => item.barcode === tag)
-      receiptMap.set(tag, { ...item, quantity: quantity } as Item)
+      const item = items.find(item => item.barcode === tag)!
+      receiptMap.set(tag, new Item(item.barcode, item.name, item.unit, item.price))
+      receiptMap.get(tag)!.quantity = quantity
     }
   }
-  console.log(...receiptMap)
-  const promotion = loadPromotions()[0] as Promotion
+  // console.log(receiptMap)
+  // const promotion = new Promotion(loadPromotions()[0]!.type, loadPromotions()[0]!.barcodes)
 
+  // let receiptArray = Array.from(receiptMap)
 
-
-
+  // console.log(`***<store earning no money>Receipt ***\n` +
+  //   `${receiptArray.map(item => item[1].toOutput() + `Subtotal: ${promotion.calculateSubtotal(item[1]).toFixed(2)}(yuan)\n`).reduce((x, y) => x + y)}` +
+  //   `----------------------\n` +
+  //   `Total: ${receiptArray.map(item => promotion.calculateSubtotal(item[1])).reduce((x, y) => x + y).toFixed(2)}(yuan)\n` +
+  //   `**********************\n`
+  // )
   return `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
@@ -45,12 +51,12 @@ export class Item {
     this.quantity = 1
   }
 
-  public toOutput() {
+  toOutput(): string {
     let unitString = ''
     if (this.quantity > 1) {
       unitString = ' ' + this.unit + 's'
     }
-    const outString = `Name: ${this.name}, Quantity: ${this.quantity + unitString}, Unit price: ${this.price.toFixed(2)} (yuan), Subtotal: ${(this.price * this.quantity).toFixed(2)} (yuan)\n`
+    const outString = `Name: ${this.name}, Quantity: ${this.quantity + unitString}, Unit price: ${this.price.toFixed(2)}(yuan), `
     return outString
   }
 }
@@ -61,5 +67,17 @@ export class Promotion {
   constructor(type: string, barcodes: string[]) {
     this.type = type
     this.barcodes = barcodes
+  }
+
+  calculateSubtotal(item: Item): number {
+    console.log(item)
+    if (this.barcodes.find(barcode => barcode === item.barcode) !== null) {
+      const promotionTimes = Math.floor(item.quantity / 3)
+      console.log(promotionTimes)
+      return item.price * 2 * promotionTimes + item.price * (item.quantity % 3)
+    }
+    else {
+      return item.price * item.quantity
+    }
   }
 }
