85057123ea03857edfb41d0f36776d02d951b303
Ziyuan <zxu25@slb.com>
2020-11-17 22:19:54 +0800

    feat: format to output receipt

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -15,25 +15,25 @@ export function printReceipt(tags: string[]): string {
       receiptMap.get(tag)!.quantity = quantity
     }
   }
-  // console.log(receiptMap)
-  // const promotion = new Promotion(loadPromotions()[0]!.type, loadPromotions()[0]!.barcodes)
+  const promotion = new Promotion(loadPromotions()[0]!.type, loadPromotions()[0]!.barcodes)
 
-  // let receiptArray = Array.from(receiptMap)
+  const receiptArray = Array.from(receiptMap)
 
-  // console.log(`***<store earning no money>Receipt ***\n` +
-  //   `${receiptArray.map(item => item[1].toOutput() + `Subtotal: ${promotion.calculateSubtotal(item[1]).toFixed(2)}(yuan)\n`).reduce((x, y) => x + y)}` +
-  //   `----------------------\n` +
-  //   `Total: ${receiptArray.map(item => promotion.calculateSubtotal(item[1])).reduce((x, y) => x + y).toFixed(2)}(yuan)\n` +
-  //   `**********************\n`
-  // )
-  return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`
+  return '***<store earning no money>Receipt ***\n' +
+    `${receiptArray.map(item => item[1].toOutput() + `Subtotal：${promotion.calculateSubtotal(item[1]).toFixed(2)}(yuan)\n`).reduce((x, y) => x + y)}` +
+    '----------------------\n' +
+    `Total：${receiptArray.map(item => promotion.calculateSubtotal(item[1])).reduce((x, y) => x + y).toFixed(2)}(yuan)\n` +
+    `Discounted prices：${(receiptArray.map(item => item[1].price * item[1].quantity).reduce((x, y) => x + y) - receiptArray.map(item => promotion.calculateSubtotal(item[1])).reduce((x, y) => x + y)).toFixed(2)}(yuan)\n` +
+    '**********************'
+
+  //   return `***<store earning no money>Receipt ***
+  // Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
+  // Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
+  // Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
+  // ----------------------
+  // Total：58.50(yuan)
+  // Discounted prices：7.50(yuan)
+  // **********************`
 }
 
 export class Item {
@@ -56,7 +56,7 @@ export class Item {
     if (this.quantity > 1) {
       unitString = ' ' + this.unit + 's'
     }
-    const outString = `Name: ${this.name}, Quantity: ${this.quantity + unitString}, Unit price: ${this.price.toFixed(2)}(yuan), `
+    const outString = `Name：${this.name}，Quantity：${this.quantity + unitString}，Unit：${this.price.toFixed(2)}(yuan)，`
     return outString
   }
 }
@@ -73,7 +73,6 @@ export class Promotion {
     console.log(item)
     if (this.barcodes.find(barcode => barcode === item.barcode) !== null) {
       const promotionTimes = Math.floor(item.quantity / 3)
-      console.log(promotionTimes)
       return item.price * 2 * promotionTimes + item.price * (item.quantity % 3)
     }
     else {
