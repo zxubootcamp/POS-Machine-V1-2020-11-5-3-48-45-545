9547f55dc8da8053bd09b79662d66113a842affe
Ziyuan <zxu25@slb.com>
2020-11-18 11:42:31 +0800

    feat: refactoring, extract function to build map for input items

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,6 +1,31 @@
 import { loadAllItems, loadPromotions } from './Dependencies'
 
 export function printReceipt(tags: string[]): string {
+  const receiptMap = mapInputItems(tags)
+  const promotion = new Promotion(loadPromotions()[0]!.type, loadPromotions()[0]!.barcodes)
+  const receiptArray = Array.from(receiptMap)
+  const originPrice = receiptArray.map(item => item[1].price * item[1].quantity).reduce((x, y) => x + y)
+  const totalPrice = receiptArray.map(item => promotion.calculateSubtotal(item[1])).reduce((x, y) => x + y)
+  const discountedPrice = originPrice - totalPrice
+
+  return '***<store earning no money>Receipt ***\n' +
+    `${receiptArray.map(item => item[1].toOutput() + `Subtotal：${promotion.calculateSubtotal(item[1]).toFixed(2)}(yuan)\n`).reduce((x, y) => x + y)}` +
+    '----------------------\n' +
+    `Total：${totalPrice.toFixed(2)}(yuan)\n` +
+    `Discounted prices：${discountedPrice.toFixed(2)}(yuan)\n` +
+    '**********************'
+
+  // return `***<store earning no money>Receipt ***\n` +
+  //   `Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)\n` +
+  //   `Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)\n` +
+  //   `Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)\n` +
+  //   `----------------------\n` +
+  //   `Total：58.50(yuan)\n` +
+  //   `Discounted prices：7.50(yuan)\n` +
+  //   `**********************`
+}
+
+export function mapInputItems(tags: string[]): Map<string, Item> {
   const items = loadAllItems()
   const receiptMap = new Map<string, Item>()
   for (const inputTag of tags) {
@@ -15,28 +40,7 @@ export function printReceipt(tags: string[]): string {
       receiptMap.get(tag)!.quantity = quantity
     }
   }
-  const promotion = new Promotion(loadPromotions()[0]!.type, loadPromotions()[0]!.barcodes)
-
-  const receiptArray = Array.from(receiptMap)
-  const originPrice = receiptArray.map(item => item[1].price * item[1].quantity).reduce((x, y) => x + y)
-  const totalPrice = receiptArray.map(item => promotion.calculateSubtotal(item[1])).reduce((x, y) => x + y)
-  const discountedPrice = originPrice - totalPrice
-
-  return '***<store earning no money>Receipt ***\n' +
-    `${receiptArray.map(item => item[1].toOutput() + `Subtotal：${promotion.calculateSubtotal(item[1]).toFixed(2)}(yuan)\n`).reduce((x, y) => x + y)}` +
-    '----------------------\n' +
-    `Total：${totalPrice.toFixed(2)}(yuan)\n` +
-    `Discounted prices：${discountedPrice.toFixed(2)}(yuan)\n` +
-    '**********************'
-
-  //   return `***<store earning no money>Receipt ***
-  // Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-  // Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-  // Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-  // ----------------------
-  // Total：58.50(yuan)
-  // Discounted prices：7.50(yuan)
-  // **********************`
+  return receiptMap
 }
 
 export class Item {
